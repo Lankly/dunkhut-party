7cced5df1852fc7cede16297a722cdae203ea9a8
Lan Betancourt <Lan@robloid.com>
2018-07-20 20:32:58 -0400

    Added tasks view, minor adjustments

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env nodemon
 var express = require('express');
 var expressLayouts = require('express-ejs-layouts');
 var app = express();
@@ -11,11 +12,14 @@ app.use(expressLayouts);
 
 
 // Resources
-  app.use('/resources', express.static(__dirname + '/resources'));
+app.use('/resources', express.static(__dirname + '/resources'));
 
 // Routes
 app.get('/', function(req, res) {
   res.render('index', { title: 'dunkhut.party' });
+})
+app.get('/tasks', function(req, res) {
+  res.render('tasks', {title: 'TODO' });
 })
 
 // Start server
