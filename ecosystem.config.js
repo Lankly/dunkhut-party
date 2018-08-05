6f17f4559f06d662db2ef1c7c15e93c531a03c36
Lan Betancourt <Lan@robloid.com>
2018-08-05 15:16:24 -0400

    Bot added to ecosystem

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,15 +1,28 @@
 module.exports = {
-  apps : [{
-    name      : 'Dunkhut-Party',
-    script    : 'index.js',
-    env: {
-      NODE_ENV: 'development'
+  apps : [
+    {
+      name      : 'Dunkhut-Party',
+      script    : 'index.js',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      },
+      watch: true,
+      ignore_watch: ['node_modules', 'resources']
     },
-    env_production : {
-      NODE_ENV: 'production'
-    },
-    watch: true,
-    ignore_watch: ['node_modules', 'resources']
-  }],
-
+    {
+      name      : 'Kaibabot',
+      script    : 'bot.js',
+      env: {
+        NODE_ENV: 'development'
+      },
+      env_production : {
+        NODE_ENV: 'production'
+      },
+      watch: ['bot.js'],
+      ignore_watch: ['node_modules', 'resources']
+    }
+  ],
 };
